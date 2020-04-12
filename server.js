d034d9788643e3c455815b0ec6075383eb1bc5ce
EsraaMamoun <esraamamoun5@gmail.com>
2020-04-12 21:00:30 +0300

    added test rout

--- server.js
+++ server.js
@@ -9,5 +9,12 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.render('pages/index');
+});
+app.use('/public', express.static('public'));
+app.get('/hello', (req,res)=>{
+    res.render('pages/index');
+});
 
 app.listen(PORT, () => console.log(`My server is up and running on ${PORT}`));
