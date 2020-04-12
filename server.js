557fcaf0f9a13c755ae63b3852a4cd41e96934fe
EsraaMamoun <esraamamoun5@gmail.com>
2020-04-12 19:50:11 +0300

    link css

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const superagent = require('superagent');
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.set('view engine', 'ejs');
+
+
+app.listen(PORT, () => console.log(`My server is up and running on ${PORT}`));
